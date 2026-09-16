ba7d0c67d5007793b1a3dea8606e6b335fbb30a1
Mihir Singh <singhmihir002@gmail.com>
2026-09-16 14:00:08 +0530

    data ingetion completed

---
+++ src/utils.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import pickle
+from sklearn.metrics import accuracy_score
+from src.exception import CustomException
+from src.logger import get_logger
+logger = get_logger(__name__)
+
+def save_object(file_path: str, obj):
+    try:
+        dir_path = os.pathdir_name(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+        logger.info(f"Object Saved Successfully at {file_path}")
+
+    except Exception as e:
+        raise CustomException(sys,e)
+
+def load_object(file_path: str):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(sys, e)
+
+def evaluate_models(x_train, y_train, x_test, y_test, models:dict):
+    try:
+        report={}
+        for model_name, model in models.items():
+            model.fit(x_train, y_train)
+            y_pred = model.predict(x_test)
+            test_accuracy = accuracy_score(y_test, y_pred)
+            report[model_name]
+            logger.info(f"{model_name} -> Test Accuracy : {test_accuracy:.2f}")
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
